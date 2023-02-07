fd6d5edbc5842a73e08a73dae81b3a5560413ed6
tugs-erdene.o <tugs-erdene.o@unitel.mn>
2023-02-07 16:49:51 +0800

    [ADD] check

--- code-with-quarkus/src/main/java/org/acme/service/GreetingService.java
+++ code-with-quarkus/src/main/java/org/acme/service/GreetingService.java
@@ -1,12 +1,26 @@
 package org.acme.service;
 
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.enterprise.context.ApplicationScoped;
+import java.util.Optional;
 import java.util.UUID;
 @ApplicationScoped
 public class GreetingService {
 
+//    @ConfigProperty(name = "greeting.name")
+//    String name;
+
+    @ConfigProperty(name = "greeting.suffix", defaultValue = "!")
+    String suffix;
+
+    @ConfigProperty(name = "greeting.prefix")
+    Optional<String> prefix;
+
     public String sayHello(){
-        return "hello";
+        final String name = ConfigProvider.getConfig().getValue("greeting.name",String.class);
+        return prefix.orElse("_") + name + suffix;
     }
     public String sayHello(String name){
         return String.format("hello %s, your id is %s", name, UUID.randomUUID().toString());
